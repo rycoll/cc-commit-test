8b653ac6b1a11541a51db86743ecd723dd71e292
No Name <colry349@gmail.com>
2023-03-16 12:23:14 +1300

    Added 2 files via CloudCannon

---
+++ cloudcannon.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+    commit_templates: [
+      {
+        template_string: "{message}{skip_ci|if=skip_checks}",
+        _inputs: {
+          skip_checks: {
+            type: "checkbox"
+          },
+          message: {
+            type: "textarea"
+          }
+        },
+        extra_data: {
+          skip_ci: "\n\n\nskip-checks:true"
+        }
+      }
+    ]
+    
+  }
